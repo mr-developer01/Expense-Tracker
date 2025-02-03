60f91d3495d5dcf07282b71a0c31e599af344bf1
mr-developer01 <rahulpratik1212@gmail.com>
2025-02-02 22:36:39 -0800

    add expense input field

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useRef } from "react";
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const desc = useRef(null);
+  const amount = useRef(null);
 
+  const submitExpance = (e) => {
+    e.preventDefault()
+    console.log(desc.current.value)
+    console.log(amount.current.value)
+    console.log("Working...")
+    desc.current.value = ""
+    amount.current.value = ""
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="main">
+      <h1>Total Expenses : Rs 15000</h1>
+      <div className="add-data">
+        <h2>Add Expense</h2>
+        <form onSubmit={submitExpance}>
+          <div className="inp-wrp">
+            <div className="description flex">
+              <label htmlFor="desc">Description</label>
+              <input ref={desc} type="text" />
+            </div>
+            <div className="amount flex">
+              <label htmlFor="desc">Amount</label>
+              <input ref={amount} type="number" />
+            </div>
+          </div>
+          <div className="submit-btn">
+            <input type="submit" />
+          </div>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
