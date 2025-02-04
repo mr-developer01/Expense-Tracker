5c9cfe48b4002caa989b6979dd9a4170517772ba
mr-developer01 <rahulpratik1212@gmail.com>
2025-02-04 01:09:45 -0800

    resolve issue - 01

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ const App = () => {
     };
 
     setExpenseData((prevData) => [...prevData, newExpense]);
-    setFilterExpense(expenseData)
+    setFilterExpense((prevData) => [...prevData, newExpense]);
     desc.current.value = "";
     amount.current.value = "";
     expType.current.value = "";
